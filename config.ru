ea48e05a871d705ee1dde1af83a66726b10c876a
Samo Kralj <samo.kralj@3fs.si>
2012-06-20 01:14:21 +0200

    Fixed request timing

--- config.ru
+++ config.ru
@@ -1,4 +1,3 @@
-$request_start_time = Time.now
 require "./settings"
 
 run MyApp.new
--- lib/sinatra/logger/request_logger.rb
+++ lib/sinatra/logger/request_logger.rb
@@ -7,10 +7,8 @@ module Sinatra
       def self.registered(app)
         # Log request method, path and parameters to log file and stdout
         app.before do
-          
-          # start time of request processing in controller 
-          @controller_request_start_time = Time.now
-          
+          @request_start_time = Time.now
+                    
           entry = case settings.environment
           when :development, :production
             "[#{Time.now}] #{request.request_method} #{request.path} with: #{params.to_json}"
@@ -24,11 +22,10 @@ module Sinatra
         end
         
         app.after do
-          total_time = (Time.now - $request_start_time)*1000
-          controller_time = (Time.now - @controller_request_start_time)*1000
+          request_time = (Time.now - @request_start_time)*1000
           entry = case settings.environment
           when :development, :production
-            "Completed #{response.status} in #{'%.2f' % total_time}ms (#{'%.2f' % controller_time}ms in controller)"
+            "Completed #{response.status} in #{'%.2f' % request_time}ms"
           else
             nil
           end
